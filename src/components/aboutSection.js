b508744669e04af91fb435d7e892235004b072d9
Muhammad Hamza <Hamza@logicons-mbp.home>
2020-09-29 14:29:17 +0500

    Modified About Section

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -22,6 +22,7 @@ const AboutSection = () => {
               uncover many web sites still in their infancy. Various versions
               have evolved over the years, sometimes by accident, sometimes on
               purpose (injected humour and the like and yeah changeg).
+              here is the commit for local branch
             </p>
             <div className="btn-row">
               <Link to="/work">View Series</Link>
