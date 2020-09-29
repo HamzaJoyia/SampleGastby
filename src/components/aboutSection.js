9cf395821b4d484500f37d1ca64c224adafe1186
Muhammad Hamza <Hamza@logicons-mbp.home>
2020-09-29 14:14:48 +0500

    New change in aboutSection

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -21,7 +21,7 @@ const AboutSection = () => {
               their default model text, and a search for ‘lorem ipsum’ will
               uncover many web sites still in their infancy. Various versions
               have evolved over the years, sometimes by accident, sometimes on
-              purpose (injected humour and the like).
+              purpose (injected humour and the like and yeah changeg).
             </p>
             <div className="btn-row">
               <Link to="/work">View Series</Link>
